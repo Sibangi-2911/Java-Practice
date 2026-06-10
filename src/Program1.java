4f72ea7c3b508b17ba6156c80ee9a19fe9c79835
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-06-10 20:32:37 +0530

    Demo Program

---
+++ src/Program1.java
@@ -0,0 +1,9 @@
+public class Program1 {
+    public static void main(String args[]){
+
+        System.out.println("Hello!!!");
+        int a = 2;
+        int b = 4;
+        System.out.println(a+b);
+    }
+}
