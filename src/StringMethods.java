d12bc436b0c1c55d4a3034ba9701862f7cdac84d
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-06-12 20:09:20 +0530

    Add String equals() method comparison examples

---
+++ src/StringMethods.java
@@ -0,0 +1,12 @@
+public class StringMethods {
+    public static void main(String args[]){
+        String str = "java";
+        String str2 = "java";
+        String str3 = "Java";
+        String str4 = "JaVa";
+        System.out.println(str.equals(str2));
+        System.out.println(str2.equals(str3));
+        System.out.println(str2.equals(str4));
+        System.out.println(str3.equals(str4));
+    }
+}
