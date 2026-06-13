3839a182b1b5807a45b19ef06511f560db61b405
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-06-13 10:39:45 +0530

    Code to combine two usernames

---
+++ src/Practice/Practice1.java
@@ -0,0 +1,13 @@
+package Practice;
+import java.util.*;
+public class Practice1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String user1, user2;
+        System.out.print("Enter your name: ");
+        user1 = sc.nextLine();
+        System.out.print("Enter your name kindly: ");
+        user2 = sc.nextLine();
+        System.out.println("Welcome "+user1+" and "+user2+" to the Company!!");
+    }
+}
