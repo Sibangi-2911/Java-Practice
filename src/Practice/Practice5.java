f68d8b9d2e6937b540cfc7f71d46c1cfd3e36b4f
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-06-13 19:42:23 +0530

    Add product name comparison using String equals method

--- src/Practice/Practice5.java
+++ src/Practice/Practice5.java
@@ -1,4 +1,14 @@
 package Practice;
-
+import java.util.*;
+//Compare product names
 public class Practice5 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String product1, product2;
+        System.out.print("Enter the name of product 1: ");
+        product1 = sc.nextLine();
+        System.out.print("Enter the name of product 2: ");
+        product2 = sc.nextLine();
+        System.out.println(product1.equals(product2));
+    }
 }
