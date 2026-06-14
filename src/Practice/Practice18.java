a3b6075062fba446749963448f5bdf7825191ec1
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-06-14 17:21:51 +0530

    Code to calculate simple interest

--- src/Practice/Practice18.java
+++ src/Practice/Practice18.java
@@ -1,4 +1,18 @@
 package Practice;
+//Simple Interest
 
+import java.util.*;
 public class Practice18 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        double principalAmount, rate, time;
+        System.out.print("Enter principal amount: ");
+        principalAmount = sc.nextDouble();
+        System.out.print("Enter rate of interest: ");
+        rate = sc.nextDouble();
+        System.out.print("Enter time period in years: ");
+        time = sc.nextDouble();
+        double simpleInterest = (principalAmount*rate*time)/100;
+        System.out.println("The SI is : "+simpleInterest);
+    }
 }
