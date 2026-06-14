8bd703aaa425a67f5efdc104f76a73a472f385b1
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-06-14 17:06:46 +0530

    Code to extract domain name

--- src/Practice/Practice16.java
+++ src/Practice/Practice16.java
@@ -1,4 +1,14 @@
 package Practice;
+//Extract Domain name
 
+import java.util.*;
 public class Practice16 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String name;
+        System.out.print("Enter a name: ");
+        name = sc.nextLine();
+        System.out.println(name.substring(4));
+        System.out.println(name.substring(4,6));
+    }
 }
