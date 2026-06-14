3e0f6721a0fda0288aa19abbc921ebf802897035
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-06-14 12:07:05 +0530

    Code to check file extension

--- src/Practice/Practice7.java
+++ src/Practice/Practice7.java
@@ -1,4 +1,13 @@
 package Practice;
+//Check file extension
 
+import java.util.*;
 public class Practice7 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String fileName;
+        System.out.print("Enter the file name with extension: ");
+        fileName = sc.nextLine();
+        System.out.println(fileName.endsWith(".java")?"java file detected":"Not a java file");
+    }
 }
