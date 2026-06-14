add09b12873c2d77bb29bac0d4ed1e0bf6bb36e9
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-06-14 12:11:44 +0530

    Add character extraction using charAt() method

--- src/Practice/Practice10.java
+++ src/Practice/Practice10.java
@@ -1,4 +1,11 @@
 package Practice;
-
+import java.util.*;
 public class Practice10 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String name;
+        System.out.print("Enter your name: ");
+        name = sc.nextLine();
+        System.out.println(name.charAt(2));
+    }
 }
