b7e4ffe45c99b6cdc9d28ca9365db8ab61275f05
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-06-14 13:58:21 +0530

    Code to calculate age

--- src/Practice/Practice13.java
+++ src/Practice/Practice13.java
@@ -1,4 +1,12 @@
 package Practice;
-
+//Calculate user age
+import java.util.*;
 public class Practice13 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int age;
+        System.out.print("Enter your current age: ");
+        age = sc.nextInt();
+        System.out.println("Age of user after 5 years will be: "+(age+5));
+    }
 }
