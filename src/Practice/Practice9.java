e8b5d05396cfa73bf62943f2f0a93a18d7c41765
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-06-14 12:08:25 +0530

    Add string replacement example using replace() method

--- src/Practice/Practice9.java
+++ src/Practice/Practice9.java
@@ -1,4 +1,12 @@
 package Practice;
-
+//Replace Name part
+import java.util.*;
 public class Practice9 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String name;
+        System.out.print("Enter the name: ");
+        name = sc.nextLine();
+        System.out.println(name.replace("Seetal", "Sibangi"));
+    }
 }
