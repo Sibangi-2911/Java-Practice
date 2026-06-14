6e96a5f1a9d8bc35d91d28341e3c8b2cc68ac0d8
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-06-14 17:06:15 +0530

    Email Validation code

--- src/Practice/Practice15.java
+++ src/Practice/Practice15.java
@@ -1,4 +1,13 @@
 package Practice;
+//Email Validation
 
+import java.util.*;
 public class Practice15 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String email;
+        System.out.println("Enter your email id to validate: ");
+        email = sc.nextLine();
+        System.out.println(email.contains("@gmail.com"));
+    }
 }
