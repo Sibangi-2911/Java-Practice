b6a7352d0ad614556d71e3b04cff4657d01f6547
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-06-18 21:34:58 +0530

    Displaying and counting all letters of a word backwards using while loop

---
+++ src/Loops/Practice4.java
@@ -0,0 +1,19 @@
+package Loops;
+// Displaying and counting all letters of a word backwards using while loop
+
+import java.util.*;
+public class Practice4 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word here: ");
+        String word = sc.next();
+        int i = word.length()-1;
+        int count = 0;
+        while(i>=0){
+            System.out.println(word.charAt(i));
+            i--;
+            count++;
+        }
+        System.out.println("Total count of letters in the word "+ word+" is: "+count);
+    }
+}
