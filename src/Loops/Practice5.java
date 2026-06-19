0288ab8560d5df5666e9d1d3055d922994fcae0c
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-06-19 22:05:38 +0530

    Code to display table of entered number

---
+++ src/Loops/Practice5.java
@@ -0,0 +1,17 @@
+package Loops;
+
+import java.util.Scanner;
+
+// Display of table of any number
+public class Practice5 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number here: ");
+        int num = sc.nextInt();
+        int i = 1;
+        while(i<=10){
+            System.out.println(i+" X "+num+" = "+i*num);
+            i++;
+        }
+    }
+}
