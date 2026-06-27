b9854a60c7195e1b80105cdee9845e944a0ace79
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-06-27 10:51:51 +0530

    Marks stored and average calculated

--- src/Arrays/Arr1.java
+++ src/Arrays/Arr1.java
@@ -1,5 +1,5 @@
 package Arrays;
-//Storing marks of students
+//Storing marks of students and average calculated
 
 import java.util.*;
 public class Arr1 {
@@ -10,8 +10,12 @@ public class Arr1 {
         for(int i=0;i<marks.length;i++){
             marks[i] = sc.nextInt();
         }
+        int sum =0;
         for(int i=0;i<marks.length;i++){
             System.out.println("The marks are : "+marks[i]);
+            sum = sum + marks[i];
         }
+        double avg = sum/marks.length;
+        System.out.print("Average = "+avg);
     }
 }
