eae73626b4c7202cb1deb5f32daf6788c92cd926
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-06-27 19:02:48 +0530

    Demonstrate array copying, filling, and binary search methods

--- src/Arrays/Arr2.java
+++ src/Arrays/Arr2.java
@@ -11,5 +11,20 @@ public class Arr2 {
         //Comparing Arrays
         int arr1[]= {100,2,33,4};
         System.out.println(Arrays.equals(arr, arr1));
+
+        //Making a copy of an array
+        int a[] = Arrays.copyOf(arr1,arr1.length);
+        System.out.println(Arrays.toString(a));
+
+        //Printing a certain range of elements from an array led to forming a new array
+        int b[] = Arrays.copyOfRange(arr1,1,arr1.length-1);
+        System.out.println(Arrays.toString(b));
+
+        //Replacing all the values of an array at once with same number
+        Arrays.fill(arr1,0);
+        System.out.println(Arrays.toString(arr1));
+
+        //Binary Search
+        System.out.println(Arrays.binarySearch(arr,33));
     }
 }
