6eef74abd8306c7397c25be2a5d2af87e35c250b
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-06-29 19:48:46 +0530

    Modified the comment

--- src/Arrays/Arr2.java
+++ src/Arrays/Arr2.java
@@ -32,7 +32,7 @@ public class Arr2 {
         System.out.println(Arrays.stream(c).sum());
         System.out.println(Arrays.stream(c).max());
 
-        //Parallel Sort
+        //Parallel Sorting
         Arrays.parallelSort(c);
         System.out.println(Arrays.toString(c));
     }
