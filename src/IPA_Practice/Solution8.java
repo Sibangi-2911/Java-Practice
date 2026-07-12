82b8c89af5805a6b68ae680c67c5829bd987e0af
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2026-07-12 17:05:18 +0530

    src/IPA_Practice/Solution8.java

---
+++ src/IPA_Practice/Solution8.java
@@ -0,0 +1,20 @@
+package IPA_Practice;
+import java.util.*;
+public class Solution8 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        int sum =0;
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+            if(arr[i]%2!=0){
+                sum+=arr[i];
+            }
+        }
+        if(sum>8 && sum<50){
+            System.out.println("Sum of odd numbers: "+sum);
+        } else {
+            System.out.println("NA");
+        }
+    }
+}
